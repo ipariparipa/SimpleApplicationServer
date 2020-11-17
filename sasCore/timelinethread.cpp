aa857cb10b3c83720dc9b4730de92fa952c8d666
NAGY, Krisztián <ipariparipa@gmail.com>
2020-11-17 20:29:24 +0100

    improve TimelineThread: handlerDescriptor, onChanged CB, get all entries

--- sasCore/timelinethread.cpp
+++ sasCore/timelinethread.cpp
@@ -5,8 +5,11 @@
 #include <map>
 #include <list>
 #include <atomic>
+#include <assert.h>
 
 #include <include/sasCore/notifier.h>
+
+#include <iostream>
 
 namespace SAS {
 
@@ -14,71 +17,139 @@ namespace SAS {
 	{
 		Notifier notif;
 
-		struct Entry
+        struct Entry : public TimelineThread::Entry
 		{
             using Ptr = std::shared_ptr<Entry>;
-			Id id = 0;
-			std::chrono::system_clock::time_point timestamp;
-            std::chrono::system_clock::duration period;
-            long remaining = 1;
+            Id id = 0;
+            std::chrono::system_clock::time_point m_timestamp;
+            std::chrono::system_clock::duration m_period;
+            long m_remaining = 1;
 			Func func;
+            std::string m_handlerDescriptor;
             bool done = false;
-            std::mutex mut;
+            std::recursive_mutex mut;
+            std::atomic_bool active;
+
+        protected:
+            std::chrono::system_clock::time_point timestamp() final override
+            {
+                std::unique_lock<std::recursive_mutex> __locaker(mut);
+                return m_timestamp;
+            }
+
+            std::chrono::system_clock::duration period() final override
+            {
+                std::unique_lock<std::recursive_mutex> __locaker(mut);
+                return m_period;
+            }
+
+            long remaining() final override
+            {
+                std::unique_lock<std::recursive_mutex> __locaker(mut);
+                return m_remaining;
+            }
+
+            std::string handlerDescriptor() final override
+            {
+                std::unique_lock<std::recursive_mutex> __locaker(mut);
+                return m_handlerDescriptor;
+            }
+
         };
 
-		std::mutex entries_mut;
+        std::mutex onChanged_mut;
+        std::function<void(const std::vector<TimelineThread::Entry::Ptr> &)> onChanged;
+        void callOnChanged()
+        {
+            std::unique_lock<std::mutex> __locker(onChanged_mut);
+            if(onChanged)
+                onChanged(getEntries());
+        }
+
+        std::recursive_mutex entries_mut;
         std::map<std::chrono::system_clock::time_point, std::list<Entry::Ptr>> entries;
         Entry::Ptr current_entry;
 
+        std::vector<TimelineThread::Entry::Ptr> getEntries()
+        {
+            std::unique_lock<std::recursive_mutex> __locaker(entries_mut);
+
+            size_t cnt = 0;
+            for(auto & t : entries)
+                cnt += t.second.size();
+
+            std::vector<TimelineThread::Entry::Ptr> ret(cnt);
+            size_t i = 0;
+            for(auto & t : entries)
+                for(auto & e : t.second)
+                {
+                    assert(i < cnt);
+                    ret[i++] = e;
+                }
+
+            return ret;
+        }
+
         Entry::Ptr takeNext()
 		{
-			std::unique_lock<std::mutex> __locker(entries_mut);
+            std::unique_lock<std::recursive_mutex> __locker(entries_mut);
 
 			while (entries.size())
 			{
 				auto it = entries.begin();
 				if (it->second.size())
                 {
-					current_entry = it->second.front();
-					it->second.pop_front();
+                    current_entry = it->second.front(); // the next one
+                    it->second.pop_front();
+                    callOnChanged();
 					return current_entry;
 				}
 				else
-                    entries.erase(it);
+                    entries.erase(it); // cleaning up
             }
 
             return current_entry = nullptr;
 		}
 
 		void restore()
-		{
-			std::unique_lock<std::mutex> __locker(entries_mut);
+        { // inserts back the current entry
+            std::unique_lock<std::recursive_mutex> __locker(entries_mut);
             if (current_entry)
             {
-                std::unique_lock<std::mutex> __e_locker(current_entry->mut);
-                entries[current_entry->timestamp].push_back(current_entry);
+                std::unique_lock<std::recursive_mutex> __e_locker(current_entry->mut);
+                entries[current_entry->m_timestamp].push_back(current_entry);
+                callOnChanged();
             }
 		}
 
         Entry::Ptr current()
 		{
-			std::unique_lock<std::mutex> __locker(entries_mut);
+            std::unique_lock<std::recursive_mutex> __locker(entries_mut);
 			return current_entry;
 		}
 
 
-        void add(TimelineThread::Id id, std::chrono::system_clock::time_point timestamp, std::chrono::system_clock::duration period, Func func, long cycle)
+        void add(TimelineThread::Id id, std::chrono::system_clock::time_point timestamp, std::chrono::system_clock::duration period, Func func, const std::string & handlerDescriptor, long cycle)
         {
-            std::unique_lock<std::mutex> __locker(entries_mut);
-
             auto e = std::make_shared<Private::Entry>();
             e->id = id;
             e->func = func;
-            e->timestamp = timestamp;
-            e->period = period;
-            e->remaining = cycle;
+            e->m_timestamp = timestamp;
+            e->m_period = period;
+            e->m_remaining = cycle;
+            e->m_handlerDescriptor = handlerDescriptor;
+            e->active = true;
 
-            entries[e->timestamp].push_back(e);
+            add(e);
+        }
+
+        void add(const Entry::Ptr & e)
+        {
+            std::unique_lock<std::recursive_mutex> __locker(entries_mut);
+
+            entries[e->m_timestamp].push_back(e);
+
+            callOnChanged();
             notif.notify();
         }
 
@@ -95,23 +166,23 @@ namespace SAS {
 		p->notif.notifyAll();
 	}
 
-    TimelineThread::Id TimelineThread::add(std::chrono::system_clock::duration timeout, Func func, long cycle)
+    TimelineThread::Id TimelineThread::add(std::chrono::system_clock::duration timeout, Func func, const std::string & handlerDescriptor, long cycle)
     {
-        return add(std::chrono::system_clock::now() + timeout, timeout, func, cycle);
+        return add(std::chrono::system_clock::now() + timeout, timeout, func, handlerDescriptor, cycle);
     }
 
-    TimelineThread::Id TimelineThread::add(std::chrono::system_clock::time_point timestamp, Func func)
+    TimelineThread::Id TimelineThread::add(std::chrono::system_clock::time_point timestamp, Func func, const std::string & handlerDescriptor)
     {
-        return add(timestamp, std::chrono::system_clock::duration(), func, 1);
+        return add(timestamp, std::chrono::system_clock::duration(), func, handlerDescriptor, 1);
     }
 
-    TimelineThread::Id TimelineThread::add(std::chrono::system_clock::time_point timestamp, std::chrono::system_clock::duration period, Func func, long cycle)
+    TimelineThread::Id TimelineThread::add(std::chrono::system_clock::time_point timestamp, std::chrono::system_clock::duration period, Func func, const std::string & handlerDescriptor, long cycle)
     {
         std::mutex mut;
         std::unique_lock<std::mutex> __locker(mut);
         static Id id(0);
 
-        p->add(++id, timestamp, period, func, cycle);
+        p->add(++id, timestamp, period, func, handlerDescriptor, cycle);
 
         resume();
 
@@ -120,36 +191,46 @@ namespace SAS {
 
 	void TimelineThread::cancel(TimelineThread::Id id)
 	{
-		std::unique_lock<std::mutex> __locker(p->entries_mut);
+        std::unique_lock<std::recursive_mutex> __locker(p->entries_mut);
         if(p->current_entry)
         {
-            std::unique_lock<std::mutex> __entry_locker(p->current_entry->mut);
-            auto __ce = p->current_entry;
-            if (p->current_entry->id == id)
+            p->current_entry->active = false; //deactivate the entry, when already/still in use
+            if(p->current_entry->mut.try_lock())
             {
-                p->current_entry->remaining = 0;
-                p->current_entry = nullptr;
-            }
-        }
-        else
-        {
-            for (auto it = p->entries.begin(); it != p->entries.end(); ++it)
-            {
-                for (auto it_l = it->second.begin(); it_l != it->second.end(); ++it_l)
+                if (p->current_entry->id == id)
                 {
-                    if ((*it_l)->id == id)
-                    {
-                        it->second.erase(it_l);
-                        if (!it->second.size())
-                            p->entries.erase(it);
-                        return;
-                    }
+                    p->current_entry->m_remaining = 0;
+                    p->current_entry = nullptr;
+                    p->notif.notify();
+                    p->current_entry->mut.unlock();
+                    return;
                 }
+                p->current_entry->mut.unlock();
             }
         }
 
-		p->notif.notify();
+        for (auto it = p->entries.begin(); it != p->entries.end(); ++it)
+        {
+            for (auto it_l = it->second.begin(); it_l != it->second.end(); ++it_l)
+            {
+                if ((*it_l)->id == id)
+                {
+                    it->second.erase(it_l);
+                    if (!it->second.size())
+                        p->entries.erase(it);
+
+                    p->callOnChanged();
+                    return;
+                }
+            }
+        }
 	}
+
+    void TimelineThread::setOnChanged(std::function<void(const std::vector<Entry::Ptr> &)> func)
+    {
+        std::unique_lock<std::mutex> __locker(p->onChanged_mut);
+        p->onChanged = func;
+    }
 
 	void TimelineThread::execute() //final override
 	{
@@ -159,37 +240,48 @@ namespace SAS {
 
             if (!e)
 				suspend();
-			else
+            else if(e->active)
 			{
-                std::chrono::system_clock::time_point timestamp;
+                std::chrono::system_clock::time_point timestamp; // the time point to be waiting for
                 {
-                    std::unique_lock<std::mutex> __e_locker(e->mut);
-                    if (std::chrono::system_clock::now() >= e->timestamp)
-                    {
-                        e->func(e->id);
-                        if(e->remaining == -1 || --e->remaining > 0)
-                            p->add(e->id, e->timestamp + e->period, e->period, e->func, e->remaining);
+                    std::unique_lock<std::recursive_mutex> __e_locker(e->mut);
+                    if (std::chrono::system_clock::now() >= e->m_timestamp)
+                    { // the entry was relevant in the past
+                        e->func(e->id, e);
+                        if(e->m_remaining == -1 || --e->m_remaining > 0)
+                        {
+                            e->m_timestamp += e->m_period;
+                            p->add(e); // append a new repetitive entry to the timeline
+                        }
                     }
                     else
-                        timestamp = e->timestamp;
+                        timestamp = e->m_timestamp;
                 }
                 if(timestamp != std::chrono::system_clock::time_point())
-                {
-                    if (p->notif.wait(timestamp))
-                    {
+                { // has to wait for
+                    if (p->notif.wait(timestamp)) // waiting for the time point of next entry
+                    { // something happened in the timeline, so let's do again
                         p->restore();
                         continue;
                     }
-                    else
-                    {
-                        std::unique_lock<std::mutex> __e_locker(e->mut);
-                        e->func(e->id);
-                        if(e->remaining == -1 || --e->remaining > 0)
-                            p->add(e->id, e->timestamp + e->period, e->period, e->func, e->remaining);
+                    else if(e->active)
+                    { // next time point reached
+                        std::unique_lock<std::recursive_mutex> __e_locker(e->mut);
+                        e->func(e->id, e);
+                        if(e->m_remaining == -1 || --e->m_remaining > 0)
+                        {
+                            e->m_timestamp += e->m_period;
+                            p->add(e); // append a new repetitive entry to the timeline
+                        }
                     }
                 }
 			}
 		}
 	}
+
+    std::vector<TimelineThread::Entry::Ptr> TimelineThread::entries()
+    {
+        return p->getEntries();
+    }
 
 }
